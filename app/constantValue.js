ee9508552685d3d7f4becddbba5cb26ccc1ffc6a
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-05-31 09:50:46 -0400

    Create constantValue.js

---
+++ app/constantValue.js
@@ -0,0 +1,7 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.seasons = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
+    exports.durations = [ ];
+});
+//# sourceMappingURL=constants.js.map
