8ef3dd834ef8b6deeb6f04c40caf7d0a8f76a278
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-01 12:02:45 -0400

    Update constantValue.js

--- app/constantValue.js
+++ app/constantValue.js
@@ -1,7 +1,7 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.seasons = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
-    exports.durations = [ ];
+    exports.years = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
+    exports.cts = [ ];
 });
 //# sourceMappingURL=constants.js.map
