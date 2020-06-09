89d6cc2a24d10ff83701cda598b92688e4a1e741
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-09 11:59:46 -0400

    Delete constantValue.js

--- app/constantValue.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.years = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
-    exports.cts = [ ];
-});
-//# sourceMappingURL=constants.js.map
